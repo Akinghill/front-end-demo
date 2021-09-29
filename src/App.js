9430dab91dc75d8182e11ed264f9389b3130012f
akinghill <austinkinghill@gmail.com>
2021-09-29 11:58:54 -0700

    fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+const axios = require("axios");
+
+const url = '/users'
+
 function App() {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    // axios
+    //   .get(url)
+    //   .then(function (response) {
+    //     // handle success
+    //     console.log(response);
+    //     setUserData(response.data);
+    //   })
+    //   .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    //   });
+    // fetching data
+
+    
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setUserData(data)
+      });
+  }, []);
+
   return (
     <div className="App">
       Hello World I'm the Front End
+      {
+        // Display Users here
+        userData.map((user) => (
+          <h1>{user.name}</h1>
+        ))
+      }
     </div>
   );
 }
